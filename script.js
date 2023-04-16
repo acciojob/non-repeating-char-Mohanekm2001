ce2157c795d7a709dede1f278f725cbb759a953f
Mohanekm2001 <mohan2001ekm@gmail.com>
2023-04-16 16:37:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-//your JS code here. If required.
-function firstNonRepeatedCharacter(str) {
-  // Create an empty object to hold the frequency of each character in the string
-  const freqObj = {};
-  
-  // Loop through each character in the string and update the frequency object
+function findFirstNonRepeatingChar(str) {
+  const charCount = {};
+
+  // loop through the string to count each character
   for (let i = 0; i < str.length; i++) {
     const char = str[i];
-    freqObj[char] = (freqObj[char] || 0) + 1;
+    charCount[char] = charCount[char] ? charCount[char] + 1 : 1;
   }
-  
-  // Loop through each character in the string again and return the first character with a frequency of 1
+
+  // loop through the string again to find the first non-repeating character
   for (let i = 0; i < str.length; i++) {
     const char = str[i];
-    if (freqObj[char] === 1) {
+    if (charCount[char] === 1) {
       return char;
     }
   }
-  
-  // If no non-repeated character is found, return null
+
+  // if no non-repeating character is found, return null
   return null;
 }
